63cb4d51db02ad020050da0e40c9efe7ff5d95a6
Robbie Heygate <r.heygate@gmail.com>
2016-01-20 22:37:56 +0000

    first push to git

---
+++ contentScript.js
@@ -0,0 +1,30 @@
+
+
+var gatherURL = {
+  videoID: [],
+  IDGather: function() {
+
+    setTimeout(function() {
+      console.log('function started');
+      console.log($('._li'));
+      function extractVideoID(url){
+        console.log('running');
+        var regExp = url.match("v=([a-zA-Z0-9\_\-]+)&?")[1];
+        var videoID = '/watch?v=' + regExp
+        return videoID;
+      }
+      $('#contentCol #contentArea #pagelet_group_ .mtm').each(function(i){
+        $(this).find('._6m3 .mbs a').trigger('click');
+        var youtubeLink = $(this).find('._6m3 .mbs').html();
+        extractVideoID(youtubeLink);
+        gatherURL.videoID.push(extractVideoID(youtubeLink));
+        console.log(this);
+        console.log(gatherURL.videoID);
+      })
+    }, 6000)
+
+
+  }
+}
+
+gatherURL.IDGather();
