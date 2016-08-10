6ff58fab10ff9ba17230c92c8145aa3b1bd76061
Robbie Heygate <r.heygate@gmail.com>
2016-08-10 17:02:05 +0100

    post method

--- popup.js
+++ popup.js
@@ -1,53 +1,61 @@
 var initiator = {
-    newLinkNumber: null,
-    accessToken: null,
-    validFBurl: function validFBurl(enteredURL) {
-        var FBurl = /^(http|https)\:\/\/www.facebook.com\/.*/i;
-        if (!enteredURL.match(FBurl)) {
-            $('.error-message').show();
-            $('.scanner-loader-container').hide();
-        } else {
-            console.log('success');
-            $('.error-message').hide();
-            chrome.tabs.executeScript(null, {
-                file: "jquery-1.11.2.min.js"
-            }, function() {
-                chrome.tabs.executeScript(null, {
-                    file: "contentScript.min.js"
-                });
-                chrome.tabs.insertCSS(null, {
-                    file: "popStyle.css"
-                });
-            });
-        }
-    },
+  newLinkNumber: null,
+  accessToken: null,
+  validFBurl: function validFBurl(enteredURL) {
+    var FBurl = /^(http|https)\:\/\/www.facebook.com\/.*/i;
+    if (!enteredURL.match(FBurl)) {
+      $('.error-message').show();
+      $('.scanner-loader-container').hide();
+    } else {
+      console.log('success');
+      $('.error-message').hide();
+      chrome.tabs.executeScript(null, {
+        file: "jquery-1.11.2.min.js"
+      }, function() {
+        chrome.tabs.executeScript(null, {
+          file: "contentScript.min.js"
+        });
+        chrome.tabs.insertCSS(null, {
+          file: "popStyle.css"
+        });
+      });
+    }
+  },
 };
 
 $(document).ready(function() {
 
-    chrome.identity.getAuthToken({
-        'interactive': true
-    }, function(token) {
-        var accessToken = token
-        setTimeout(function() {
-            chrome.runtime.onConnect.addListener(function(port) {
-                port.postMessage({
-                    greeting: 'hello',
-                    accessToken: accessToken,
-                });
-            });
+  $.ajax({
+    method: "POST",
+    url: "http://139.59.190.164:3000",
+  }).done(function(data, textStatus, request) {
+    console.log('done');
+  });
 
-        }, 100);
 
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tab) {
-            //Be aware that `tab` is an array of Tabs
-            console.log(tab[0].url);
-            console.log('working');
-            initiator.validFBurl(tab[0].url);
+  chrome.identity.getAuthToken({
+    'interactive': true
+  }, function(token) {
+    var accessToken = token
+    setTimeout(function() {
+      chrome.runtime.onConnect.addListener(function(port) {
+        port.postMessage({
+          greeting: 'hello',
+          accessToken: accessToken,
         });
+      });
 
+    }, 100);
+
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tab) {
+      //Be aware that `tab` is an array of Tabs
+      console.log(tab[0].url);
+      console.log('working');
+      initiator.validFBurl(tab[0].url);
     });
+
+  });
 });
